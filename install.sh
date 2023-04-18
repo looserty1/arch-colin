e25d7822735c89b9ba7a07217f4070ca00bc544a
Looserty <115899596+looserty1@users.noreply.github.com>
2023-04-18 10:14:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ mkdir -p ~/.config/{bspwm,sxhkd,polybar}
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 chmod u+x ~/.config/bspwm/bspwmrc
-cd polybar
-touch launch.sh
+cd ~/.config/polybar
+cp ~/arch-colin/launch.sh ./
 chmod +x launch.sh
+echo "$HOME/.config/polybar/launch.sh" >> ~/.config/bspwm/bspwmrc
+cp /etc/polybar/config.ini ~/.config/polybar
+systemctl enable lightdm
+systemctl set-default graphical.target
+systemctl enable lightdm --now
