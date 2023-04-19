bdb4cf24cbda0138b7c6d3109850ac91ff17c0ca
Looserty <115899596+looserty1@users.noreply.github.com>
2023-04-19 18:25:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 sudo pacman -S bspwm sxhkd lightdm polybar gnome-terminal firefox lightdm-gtk-greeter xorg
-mkdir ~/.config
-mkdir -p ~/.config/{bspwm,sxhkd,polybar}
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
-chmod u+x ~/.config/bspwm/bspwmrc
-cd ~/.config/polybar
-cp ~/arch-colin/launch.sh ./
+mkdir $HOME/.config
+mkdir -p $HOME/.config/{bspwm,sxhkd,polybar}
+cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm/
+cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd/
+chmod u+x $HOME/.config/bspwm/bspwmrc
+cd $HOME/.config/polybar
+cp $HOME/arch-colin/launch.sh ./
 chmod +x ~/.config/polybar/launch.sh
-echo "$HOME/.config/polybar/launch.sh" >> ~/.config/bspwm/bspwmrc
-cp /etc/polybar/config.ini ~/.config/polybar
+echo "$HOME/.config/polybar/launch.sh" >> $HOME/.config/bspwm/bspwmrc
+cp /etc/polybar/config.ini $HOME/.config/polybar
 systemctl enable lightdm
 systemctl set-default graphical.target
 systemctl enable lightdm --now
