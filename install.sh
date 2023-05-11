b7a59fd83260f9415761ef7f9ac7cfd881932528
Looserty <115899596+looserty1@users.noreply.github.com>
2023-05-11 15:02:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "Write your home directory for your user"
 read $HOME
-sudo pacman -S bspwm sxhkd lightdm polybar gnome-terminal firefox lightdm-gtk-greeter xorg
+sudo pacman -S bspwm sxhkd lightdm polybar gnome-terminal firefox lightdm-gtk-greeter xorg dmenu
 sudo mkdir $HOME/.config
 sudo mkdir -p $HOME/.config/{bspwm,sxhkd,polybar}
 sudo cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm/
@@ -8,8 +8,8 @@ sudo cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd/
 sudo chmod u+x $HOME/.config/bspwm/bspwmrc
 cd $HOME/.config/polybar
 sudo cp $HOME/arch-colin/launch.sh ./
-sudo chmod +x ~/.config/polybar/launch.sh
-sudo echo "$HOME/.config/polybar/launch.sh" >> $HOME/.config/bspwm/bspwmrc
+sudo chmod +x $HOME/.config/polybar/launch.sh
+sudo echo "$HOME/.config/polybar/launch.sh &" >> $HOME/.config/bspwm/bspwmrc
 sudo cp /etc/polybar/config.ini $HOME/.config/polybar
 sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
